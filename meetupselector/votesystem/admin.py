8ab0a3b494ab426ad718ea2abeaf141a7bbf023d
serfer2 <serfer2@gmail.com>
2020-10-10 22:26:05 +0200

    feat: adds TopicProposal to django admin

--- meetupselector/votesystem/admin.py
+++ meetupselector/votesystem/admin.py
@@ -1,4 +1,27 @@
 from django.contrib import admin
+
+from meetupselector.secretballot import enable_voting_on
 from .models import TopicProposal
 
-admin.site.register(TopicProposal)
+
+class TopicProposalAdmin(admin.ModelAdmin):
+
+    model = TopicProposal
+    list_display = (
+        'topic',
+        'level',
+        'description',
+        'date_added',
+        'votes'
+    )
+    readonly_fields = ('vote_total', 'date_added')
+
+    def __init__(self, *args, **kwargs):
+        super(TopicProposalAdmin, self).__init__(*args, **kwargs)
+        enable_voting_on(TopicProposal)
+
+    def votes(self, obj):
+        return obj.vote_total
+
+
+admin.site.register(TopicProposal, TopicProposalAdmin)
