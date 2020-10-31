ebbed2a9797864d444df70a884d0aef85e3b7ba9
Alejandro <alejandroalmira@protonmail.com>
2020-10-31 18:53:37 +0100

    feat: add content to presentation

--- meetupselector/landingpage/models.py
+++ meetupselector/landingpage/models.py
@@ -21,7 +21,7 @@ class Content(models.Model):
     date_added = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ["-date_added"]
+        ordering = ["date_added"]
 
     def __str__(self):
         return self.title
