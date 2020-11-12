941bf3da3618c058bd584ca190175555b45db3e1
BirdOnTheBranch <planelles.luis@gmail.com>
2020-11-12 19:16:09 +0100

    fix(Docker): Add docker-compose and optimize dockerfile.

--- Dockerfile
+++ Dockerfile
@@ -7,8 +7,3 @@ RUN pip install --no-cache-dir -r requirements.txt \
     && mkdir /usr/src/app/tmp \
     && chmod +x docker-entrypoint.sh
 
-VOLUME ["/usr/src/app/tmp"]
-
-EXPOSE 8000
-
-CMD ["./docker-entrypoint.sh"]
--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -6,6 +6,6 @@ python manage.py collectstatic --noinput
 
 # Apply database migrations
 echo "Apply database migrations"
-python manage.py migrate --noinput
+python manage.py migrate 
 
 exec python manage.py runserver 0.0.0.0:8000
